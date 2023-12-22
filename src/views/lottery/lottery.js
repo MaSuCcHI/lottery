97a5242c1962065532185420131452cba3286771
keisuke.masuda <keiske.masuda@gmail.com>
2023-12-23 00:29:19 +0900

    update

--- src/views/lottery/lottery.js
+++ src/views/lottery/lottery.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Button from "@mui/material/Button"
-import { Card, CardContent, Typography } from '@mui/material';
+import Radio from "@mui/material/Radio"
+import { Card, CardContent, FormControl, FormControlLabel, FormLabel, RadioGroup, Typography } from '@mui/material';
+import shortSound from '../../sound/short.mp3';
 import middileSound from '../../sound/middle.mp3';
+import longSound from '../../sound/long.mp3';
 
 function getRandom(arr, nums=1) {
   var result = new Array(nums), 
@@ -20,14 +23,26 @@ function getRandom(arr, nums=1) {
 function getBlankUsers(nums=1) {
   var result = new Array(nums);
   while (nums--) {
-    result[nums] = {name: "　", department: "　"};
+    result[nums] = {name: "　", department: "　", reading: "　"};
   }
   return result;
 }
 
-function playDrumSound(l=1) {
-  const audio = new Audio(middileSound);
-  audio.volume = 0.5;
+function playDrumSound(l) {
+  let audio;
+  console.log(l);
+  if (l === "short") {
+    audio = new Audio(shortSound);
+  } else if (l === "middle") {
+    audio = new Audio(middileSound);
+  } else if (l === "long") {
+    audio = new Audio(longSound);
+  } else {
+    audio = new Audio(middileSound);
+    console.log("error");
+  }
+
+  audio.volume = 1.0;
   audio.play();
 }
 
@@ -37,6 +52,7 @@ export function Lottery(
     const [nowGift, setNowGift] = useState(gifts[0]);
     const randomIndex = Math.floor(Math.random() * users.length);
     const [selectedUsers, setSelectedUsers] = useState([users[randomIndex]]);
+    const [drumrollLength, setDrumrollLength] = useState("middle");
 
     const [ disabledStartLottery, setDisabledStartLottery ] = useState(false);
      useEffect(() => {
@@ -61,17 +77,22 @@ export function Lottery(
         抽選会
       </h1>
       <div style={{height:"90%", width:"100%", display: "flex", flexDirection: 'column', alignItems: "center", justifyContent: "center"}}>
-        <Card sx={{ minWidth: "50%", margin: 30}}>
+        <Card sx={{ minWidth: "50%"}}>
             <CardContent> 
-              {nowGift.gift} {nowGift.unit}名様
+              <Typography variant="h3" component="div">
+                {nowGift.gift} {nowGift.unit}名様
+              </Typography>
              </CardContent> 
             {selectedUsers.map((user, index) => (
                 <CardContent>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    <Typography variant="h5" color="text.secondary">
                         {user.department}
                     </Typography>
-                    <Typography variant="h5" component="div">
+                    <Typography variant="h4" component="div">
                         {user.name}
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        {user.reading}
                     </Typography>
                 </CardContent>
             ))}
@@ -84,14 +105,15 @@ export function Lottery(
           disabled={disabledStartLottery}
           variant="contained"
           onClick={()=>{
-            playDrumSound(); 
+            playDrumSound(drumrollLength); 
+            const soundLength = drumrollLength === "short" ? 1750 : drumrollLength === "middle" ? 3900 : 5900;
             const interval = setInterval(() => {
               setSelectedUsers(getRandom(users, nowGift.unit));
             }, 50);
             setTimeout(() => {
               clearInterval(interval);
               setDisabledStartLottery(true);
-            }, 3900);
+            }, soundLength);
           }}> 
                 抽選開始
         </Button>
@@ -126,6 +148,20 @@ export function Lottery(
             終了
         </Button> 
         }
+        <FormControl style={{left: 10}}>
+          <RadioGroup
+            row
+            value={drumrollLength}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setDrumrollLength(e.target.value);
+            }}
+            >
+            <FormControlLabel value="short" control={<Radio />} label="short" />
+            <FormControlLabel value="middle" control={<Radio />} label="middle" />
+            <FormControlLabel value="long" control={<Radio />} label="long" />
+          </RadioGroup>
+        </FormControl>
     </div>
   );
 }
